1c5278d6f78cf1ff596e4768ebd4262ca56398c4
Tanya Maxar <kolt.levry@gmail.com>
2025-02-12 13:23:00 +0100

    start learn ts from 0 to up with new cohort groupe

---
+++ Typescript_Intro/script.js
@@ -0,0 +1,12 @@
+// npm = Node Package Manager
+var isLoading = false;
+isLoading = true;
+console.log("Is loading: ", isLoading);
+// type Status = 'loading' | 'success' | 'error';
+var stat = "loading";
+// typeof - function
+console.log("Type of (stat): ", typeof (stat));
+Array.isArray(stat);
+console.log("Is array - stat: ", Array.isArray(stat));
+// modern option to use of typeOf
+console.log("Type of (Err): ", typeof isLoading);
---
+++ Typescript_Intro/script.ts
@@ -0,0 +1,51 @@
+// npm = Node Package Manager
+let isLoading: boolean = false;
+isLoading = true;
+
+console.log("Is loading: ", isLoading);
+
+// типізація по значенню
+type Loading = "loading";
+type Success = "success";
+type Err = "error";
+
+type Status = Loading | Success | Err;
+// type Status = 'loading' | 'success' | 'error';
+
+let stat: Status = "loading"; 
+
+// typeof - function
+console.log("Type of (stat): ", typeof(stat));
+
+Array.isArray(stat);
+console.log("Is array - stat: ", Array.isArray(stat));
+
+// modern option to use of typeOf
+console.log("Type of (Err): ", typeof isLoading);
+
+interface IPersone {
+    name: string,
+    isProgrammer?: boolean;
+}
+
+interface Employee extends IPersone{
+    salary: number;
+}
+
+const employ1: Employee = {
+    salary: 2200,
+    name: "Jack",
+    isProgrammer: true,
+};
+// too big / long
+// const newEmployee: {
+//     name: string;
+//     salary: number;
+//     isProgrammer?: boolean;
+// } = {
+//     name: 'John',
+//     salary: 2500,
+// };
+
+// type - часто для примітивів і можна обєднувати як в 12 строчці, але не описує функції, це робить інтерфейс
+// inetrface - зачасту працюють з обєктами
